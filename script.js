22a816620176fd06852139c34e78dff3fbc57206
Collin <collin.estlee@gmail.com>
2023-10-15 09:39:31 +0800

    App Logic - Task completed & Task delete

--- script.js
+++ script.js
@@ -1,105 +1,157 @@
+// Array for todo List
+const todo = []
+let todoDOM = []
+
+// DOM
 const addBtn = document.querySelector("#add-todo-btn")
-const delBtn = document.querySelectorAll(".del-btn")
+
 const newTodo = document.querySelector(".new-todo")
 const tasklist = document.querySelector(".tasklist")
 const rating = document.querySelector(".priority-num")
 let task = document.querySelectorAll(".task")
 const showCompletedBox = document.querySelector("#showCompleted")
-let completeBtn = document.querySelectorAll(".complete-btn")
 
+
+// Add Button
 addBtn.addEventListener("click", (e)=>{
     e.preventDefault()
     addTodo()
+    
 })
 
-
-for (let i = 0; i < delBtn.length; i++) {
-    delBtn[i].addEventListener("click", () => {
-        task[i].remove();
-    });
-}
-
-
-
+// Add Function
 function addTodo(){
     const value = newTodo.value;
     if (value == "") {
         return;
     }
-    let newTask = document.createElement('div')
-    newTask.classList.add("task")
-    newTask.innerHTML =
-        `<p class="todo">${value}</p>
-    <span class="importance"> Importance: </span>
-    <select name="priority" class="priority-num">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-    </select>
-    <button class="complete-btn">Complete</button>
-    <img class="del-btn" src="./Asset/icons8-trash.png">`;
-    tasklist.append(newTask);
-    newTodo.value = "";
 
-    // Attach event listener to the new complete button
-    const completeBtn = newTask.querySelector(".complete-btn");
-    completeBtn.addEventListener("click", () => {
-       newTask.classList.toggle("completed")
-
-    //    Check for Show completed mark
-       showComplete(showCompletedBox)
-});
+    todo.push({title: value, isCompleted : false})
+    console.log(todo);
+    newTodo.value=""
+    syncTodo()
 }
 
 
 
+// To sync Todo array with DOM 
+function syncTodo() {
 
+  // Clear existing tasks
+  tasklist.innerHTML = "";
+  
+  //ToDo list for DOM
+  todoDOM = []
+  // Check for completed/filter
+      for(i = 0; i < todo.length; i++ )
+      {
+        if(todo[i].isCompleted && !showCompletedBox.checked){
+          continue
+        } else if(todo[i].isCompleted && showCompletedBox.checked || !todo[i].isCompleted){
+          todoDOM.push(todo[i])
+        }
+      }
+  
+  // DOM Loop
+      for(j = 0; j < todoDOM.length; j++ ){
+
+
+        let newTask = document.createElement('div');
+        newTask.classList.add("task");
+
+        newTask.innerHTML =
+          `<p class="todo">${todoDOM[j].title}</p>
+          <span class="importance"> Importance: </span>
+          <select for="importance"> 
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button class="complete-btn">Complete</button>
+          <img class="del-btn" src="./Asset/icons8-trash.png">`;
+
+        if (todoDOM[j].isCompleted === true && showCompletedBox.checked){
+            newTask.classList.add("completed")
+          }  
+        tasklist.append(newTask);
+      }
+      addCompleteBtnsEvent()
+      addDelBtnsEvent()
+};
+
+// Add event listener for all complete buttons
+function addCompleteBtnsEvent(){
+let completeBtns = document.querySelectorAll(".complete-btn");
+  completeBtns.forEach((completeBtn, i) => {
+    completeBtn.addEventListener("click", () => {
+      toggleComplete(i);
+      console.log("Complete btn pressed");
+});})
+}
+  
+// Add event listener for all complete buttons
+function addDelBtnsEvent() {
+  const delBtn = document.querySelectorAll(".del-btn")
+  delBtn.forEach((delBtn, index) => {
+    delBtn.addEventListener("click", () => {
+      {
+        for (let j = 0; j < todo.length; j++) {
+          if (todoDOM[index].title === todo[j].title) {
+            todo.splice(j, 1);
+            todoDOM.splice(index, 1);
+          }
+        }
+      }
+      syncTodo();
+      console.log("DelButton pressed");
+    });
+  });
+}
+
+  // function delTodo(index){
+  //   console.log("delBtn pressed");
+  //   for(i = 0; i < todoDOM.length; i++){
+  //     for(j = 0; j < todo.length; j++){
+  //     if(todoDOM[index].title === todo(j).title){
+  //       todo.splice(i,1)
+  //       todoDOM.splice(i,1)
+  //     }}
+  //   syncTodo()
+  //   }
+  // }
+
+function toggleComplete(i){
+  console.log(todoDOM[i]);
+    let task = document.querySelectorAll('.task')
+    if(todoDOM[i].isCompleted === false){
+        todoDOM[i].isCompleted = true
+        task[i].classList.add("completed")
+        syncTodo()
+    } else
+if(todoDOM[i].isCompleted === true)
+    {todoDOM[i].isCompleted = false
+    task[i].classList.remove("completed")}
+    syncTodo()
+}
 
 
 // Hide Completed <-> Show completed
 // If completed === true && checkBox === true => class + hide
 
 showCompletedBox.addEventListener("click", ()=>{
- const value = showCompletedBox
-    showComplete(value)
+   syncTodo()
 })
 
-function showComplete(value){
-    if(value.checked){
-        let task = document.getElementsByClassName("task ")
-    for(i = 0; i < task.length;i++){
-        if(task[i].classList.contains("completed")){
-            task[i].classList.remove("hidden")
-        }
-        }
-    };
-    
 
-    if(!value.checked){
-        let task = document.getElementsByClassName("task ")
-    for(i = 0; i < task.length;i++){
-        if(task[i].classList.contains("completed")){
-            task[i].classList.add("hidden")
-        }
-        }
-    };
-}
-
-   
-
-   
-    
-    // if(!value.checked){
-    //     for(let i = 0; i < tasklist.childElementCount; i++){
-    //         if (tasklist.children.contains("completed")){
-    //             tasklist.children.classList.toggle("hidden")
-    //         }
-    //     }
-    // }
-// }
-
-
-// Ranking
+// Rating change
 // Trash
+
+// write to do -> update array object
+// for every todo -> sync -> append html
+// 
+
+window.addEventListener("load", ()=>{
+    syncTodo()
+})
